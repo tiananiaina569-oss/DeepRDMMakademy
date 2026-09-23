9f1690249cc469e8f6848472deb2c5ef58807d14
tiananiaina569-oss <tiananiaina569@gmail.com>
2026-09-23 15:36:20 +0300

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// ============================================================
+// DeepRDMMakademy — Configuration Firebase
+// ============================================================
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDhZontZCd8bstyUcNY-cW_PdrGm87WvLQ",
+    authDomain: "deeprdmmakademy.firebaseapp.com",
+    projectId: "deeprdmmakademy",
+    storageBucket: "deeprdmmakademy.firebasestorage.app",
+    messagingSenderId: "900106476108",
+    appId: "1:900106476108:web:c9e52848ed33080a13f2e9",
+    measurementId: "G-L4KXE7CS3D"
+};
+
+export { firebaseConfig };
